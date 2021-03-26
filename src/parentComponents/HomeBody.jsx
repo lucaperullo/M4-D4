847ae4c9d5bce36287de930fe15cd8b2b54cf295
lucaperullo <72197904+lucaperullo@users.noreply.github.com>
2021-03-26 12:07:35 +0100

    splide

---
+++ src/parentComponents/HomeBody.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Slide from "../components/Slide";
+class HomeBody extends React.Component {
+  render() {
+    return (
+      <>
+        <Slide />
+      </>
+    );
+  }
+}
+export default HomeBody;
